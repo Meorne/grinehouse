bdb8d6c68fe1b57d4445dee85c36815fa23d5fc7
Stéphane Comte <scomte@splio.com>
2024-03-08 15:38:38 +0100

    add google analytics

---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `grine-house.firebaseapp.com`,
+  projectId: `grine-house`,
+  storageBucket: `grine-house.appspot.com`,
+  messagingSenderId: `183619162423`,
+  appId: `1:183619162423:web:319f5564a70d0ca81e0ab2`,
+  measurementId: `G-FPRZ3DT193`,
+};
+
+// Initialize Firebase
+const app = firebaseConfig?.apiKey ? initializeApp(firebaseConfig) : undefined;
+export const analytics = app && getAnalytics(app);
