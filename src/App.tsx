b283a2f1ac5a635a4c4f9f3ddfcb1c8ced6aada3
Stéphane Comte <scomte@splio.com>
2024-03-08 11:58:54 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grid, Paper } from '@mui/material';
+import './App.scss';
+import { ReactComponent as Grineouille } from './assets/grinouillelogo.svg';
 
-function App() {
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{ flexGrow: 1, width: 1024, margin: `0 auto` }}
+      alignItems="center"
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Grineouille />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Grineouille />
+        </Grid>
+      </Grid>
+    </Box>
   );
-}
+};
 
 export default App;
